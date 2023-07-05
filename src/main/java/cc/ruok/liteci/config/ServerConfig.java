b5a9b3bbdc5793f0b91ec32dd11cec9de9fa1d4d
Administrator <i@ruok.cc>
2023-07-05 22:59:36 +0800

    界面设置页面

--- src/main/java/cc/ruok/liteci/config/ServerConfig.java
+++ src/main/java/cc/ruok/liteci/config/ServerConfig.java
@@ -8,6 +8,8 @@ public class ServerConfig extends Config {
 
     public int http_port = 80;
     public String title = "LiteCI";
+    public String theme = "indigo";
+    public String accent = "pink";
 
     @Override
     public File getFile() {
